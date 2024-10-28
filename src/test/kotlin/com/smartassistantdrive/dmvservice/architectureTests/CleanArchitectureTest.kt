759bf8ec29025e4aba6ab4d8028f4b2bddbdee5f
DiLilloDaniele <daniele.dilillo@studio.unibo.it>
2024-10-28 11:47:23 +0100

    test: added new architectural and business tests

---
+++ src/test/kotlin/com/smartassistantdrive/dmvservice/architectureTests/CleanArchitectureTest.kt
@@ -0,0 +1,40 @@
+package com.smartassistantdrive.dmvservice.architectureTests
+
+import com.tngtech.archunit.core.importer.ClassFileImporter
+import com.tngtech.archunit.library.Architectures.layeredArchitecture
+import kotlin.test.Test
+
+class CleanArchitectureTest {
+
+	@Test
+	fun layerDependencies() {
+		val importedClasses = ClassFileImporter().importPackages("com.smartassistantdrive.dmvservice")
+		val cleanArchitecture = layeredArchitecture()
+			.consideringOnlyDependenciesInLayers()
+			.layer("Domain").definedBy("..domainLayer..")
+			.layer("Business").definedBy("..businessLayer..")
+			.layer("Adapter").definedBy("..interfaceAdaptersLayer..")
+			.whereLayer("Adapter").mayNotBeAccessedByAnyLayer()
+			.whereLayer("Business").mayOnlyBeAccessedByLayers("Adapter")
+			.whereLayer("Domain").mayOnlyBeAccessedByLayers("Business", "Adapter")
+
+		cleanArchitecture.check(importedClasses)
+	}
+
+	@Test
+	fun everythingPassThroughBusinessLayer() {
+		val importedClasses = ClassFileImporter().importPackages("com.smartassistantdrive.dmvservice")
+		val ruleEverythingPassThroughBusinessLayer = layeredArchitecture()
+			.consideringOnlyDependenciesInLayers()
+			.layer("Business").definedBy("..businessLayer..")
+			.layer("Controller").definedBy("..interfaceAdaptersLayer.controllers..")
+			.layer("Persistence").definedBy("..interfaceAdaptersLayer.persistence..")
+			// .layer("Security").definedBy("..interfaceAdaptersLayer.security..")
+			.whereLayer("Persistence").mayNotBeAccessedByAnyLayer()
+			.whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+			// .whereLayer("Security").mayNotBeAccessedByAnyLayer()
+			.whereLayer("Business").mayOnlyBeAccessedByLayers("Controller", "Persistence")
+
+		ruleEverythingPassThroughBusinessLayer.check(importedClasses)
+	}
+}
